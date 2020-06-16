901b519ef15be76efc027708359710c2d2b67743
claudiobailon <c.bailonschubert@gmail.com>
2020-06-15 17:40:26 -0700

    Added event listener and event handler

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-//=============================Global Variables===================================
+//=============================Global Variables & Functions===================================
 var productCollection = [];
 var totalClicks = 0;
 var maxClicks = 5;
 
+function pickRandom(min, max){
+  return Math.floor(Math.random() * (max - min) + min);
+}
 //=============================Contructor=========================================
 function Product(imageSrc, caption){
   this.imageSrc = imageSrc;
@@ -23,18 +26,46 @@ new Product('img/bathroom.jpg', 'Bathroom Stand');
 new Product('img/boots.jpg', 'Open-Toed Rain boots');
 new Product('img/breakfast.jpg', 'All-In-One Breakfast Maker');
 new Product('img/bubblegum.jpg', 'Bubblegum Flavored Meatballs');
-new Product('img/chair.jpg', 'Mounded Chair');
-new Product('img/cthulhu.jpg', 'Cthulhu Action Figure');
-new Product('img/dog-duck.jpg', 'Duck Dog Mask');
-new Product('img/dragon.jpg', 'Canned Dragon Meat');
-new Product('img/pen.jpg', 'Utensil-Capped Pen');
-new Product('img/pet-sweep.jpg', 'Pet Sweep');
-new Product('img/scissors.jpg', 'Pizza Scissors');
-new Product('img/shark.jpg', 'Shark Sleeping Bag');
-new Product('img/sweep.png', 'Baby Sweeper');
-new Product('img/tauntaun.jpg', 'Tauntaun Sleeping bag');
-new Product('img/unicorn.jpg', 'Canned Unicorn Meat');
-new Product('img/usb.gif', 'Tentacle Flash Drive');
-new Product('img/water-can.jpg', 'Unique Watering-Can');
-new Product('img/wine-glass.jpg', 'Special Wine Glass');
+// new Product('img/chair.jpg', 'Mounded Chair');
+// new Product('img/cthulhu.jpg', 'Cthulhu Action Figure');
+// new Product('img/dog-duck.jpg', 'Duck Dog Mask');
+// new Product('img/dragon.jpg', 'Canned Dragon Meat');
+// new Product('img/pen.jpg', 'Utensil-Capped Pen');
+// new Product('img/pet-sweep.jpg', 'Pet Sweep');
+// new Product('img/scissors.jpg', 'Pizza Scissors');
+// new Product('img/shark.jpg', 'Shark Sleeping Bag');
+// new Product('img/sweep.png', 'Baby Sweeper');
+// new Product('img/tauntaun.jpg', 'Tauntaun Sleeping bag');
+// new Product('img/unicorn.jpg', 'Canned Unicorn Meat');
+// new Product('img/usb.gif', 'Tentacle Flash Drive');
+// new Product('img/water-can.jpg', 'Unique Watering-Can');
+// new Product('img/wine-glass.jpg', 'Special Wine Glass');
+
+//==============================Create Event Listener=============================
+
+var catalogImageSection = document.getElementById('catalog-images');
+catalogImageSection.addEventListener('click','handleClickOnProduct');
+
+//===============================Callback Funtion==================================
+function handleClickOnProduct(event){
+  if(event.target.tagname === 'IMG'){
+    totalClicks++;
+
+    //From demo,  uses src to find correct product that has been clicked, then adds one to that product's clicked count
+    var targetSrc = event.target.getAttribute('src');
+    for (var i = 0; i < productCollection.length; i++){
+      if (productCollection[i].imageSrc === targetSrc){
+        productCollection[i].clicked++;
+      }
+    }
+    //When totatClicks === maxClicks, this removes event listener
+    if(totalClicks === maxClicks){
+      catalogImageSection.removeEventListener('click','handleClickOnProduct');//stops event listener
+    }
+    reRenderRandomImages();
+  }
+}
+
+//===============================ReRender Images after the click=====================
+function
 
