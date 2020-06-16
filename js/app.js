394b30ac0f0a36a511bbf3402e34c6ade83b93ac
claudiobailon <c.bailonschubert@gmail.com>
2020-06-15 21:57:14 -0700

    Replaced images with list when maxClicks reached

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //=============================Global Variables & Functions===================================
 var productCollection = [];
 var totalClicks = 0;
-var maxClicks = 5;
+var maxClicks = 25 ;
 
 function pickRandom(min, max){
   return Math.floor(Math.random() * (max - min) + min);
@@ -26,20 +26,20 @@ new Product('img/bathroom.jpg', 'Bathroom Stand');
 new Product('img/boots.jpg', 'Open-Toed Rain boots');
 new Product('img/breakfast.jpg', 'All-In-One Breakfast Maker');
 new Product('img/bubblegum.jpg', 'Bubblegum Flavored Meatballs');
-// new Product('img/chair.jpg', 'Mounded Chair');
-// new Product('img/cthulhu.jpg', 'Cthulhu Action Figure');
-// new Product('img/dog-duck.jpg', 'Duck Dog Mask');
-// new Product('img/dragon.jpg', 'Canned Dragon Meat');
-// new Product('img/pen.jpg', 'Utensil-Capped Pen');
-// new Product('img/pet-sweep.jpg', 'Pet Sweep');
-// new Product('img/scissors.jpg', 'Pizza Scissors');
-// new Product('img/shark.jpg', 'Shark Sleeping Bag');
-// new Product('img/sweep.png', 'Baby Sweeper');
-// new Product('img/tauntaun.jpg', 'Tauntaun Sleeping bag');
-// new Product('img/unicorn.jpg', 'Canned Unicorn Meat');
-// new Product('img/usb.gif', 'Tentacle Flash Drive');
-// new Product('img/water-can.jpg', 'Unique Watering-Can');
-// new Product('img/wine-glass.jpg', 'Special Wine Glass');
+new Product('img/chair.jpg', 'Mounded Chair');
+new Product('img/cthulhu.jpg', 'Cthulhu Action Figure');
+new Product('img/dog-duck.jpg', 'Duck Dog Mask');
+new Product('img/dragon.jpg', 'Canned Dragon Meat');
+new Product('img/pen.jpg', 'Utensil-Capped Pen');
+new Product('img/pet-sweep.jpg', 'Pet Sweep');
+new Product('img/scissors.jpg', 'Pizza Scissors');
+new Product('img/shark.jpg', 'Shark Sleeping Bag');
+new Product('img/sweep.png', 'Baby Sweeper');
+new Product('img/tauntaun.jpg', 'Tauntaun Sleeping bag');
+new Product('img/unicorn.jpg', 'Canned Unicorn Meat');
+new Product('img/usb.gif', 'Tentacle Flash Drive');
+new Product('img/water-can.jpg', 'Unique Watering-Can');
+new Product('img/wine-glass.jpg', 'Special Wine Glass');
 
 //==============================Create Event Listener=============================
 
@@ -77,6 +77,8 @@ function reRenderRandomImages(){
   }
   while(thirdRandomImage === firstRandomImage || thirdRandomImage === secondRandomImage){//Makes certain no images are repeated on this render
     thirdRandomImage = pickRandom(0, productCollection.length);
+
+    //ToDO: Make it so pictures are different each time
   }
 
   var leftImage = document.getElementById('left-image');
@@ -100,6 +102,35 @@ function reRenderRandomImages(){
   rightImage.src= thirdProduct.imageSrc;
   rightText.textContent = thirdProduct.imageCaption;
   thirdProduct.shown++;
-}
 
+  //Should be ablt to put lines 89-102 in a loop
+  if(totalClicks === maxClicks){
+    //the below section hides the images to display the list. Learned from https://stackoverflow.com/questions/6802683/js-how-to-remove-image-with-javascript
+    document.getElementById('left').style.display='none';
+    document.getElementById('center').style.display='none';
+    document.getElementById('right').style.display='none';
+    document.getElementById('intro').style.display='none';
+
+    var resultsList = document.getElementById('catalog-images');
+    var listHeader = document.createElement('h3');
+    var listContent = document.createElement('li');
+
+    listHeader.textContent = 'Survey Results';
+    resultsList.appendChild(listHeader);
+
+    for(var i = 0; i < productCollection.length; i++){
+
+      listContent = document.createElement('li');
+      listContent.textContent = 'The image "' + productCollection[i].imageCaption + '" recieved ' + productCollection[i].clicked + ' votes out of ' + productCollection[i].shown + ' times shown.';
+      console.log(listContent);
+      resultsList.appendChild(listContent);
+
+      //this if statement is just to make it read better if a product recieved one vote. Vote vs votes.
+      if(productCollection[i].clicked === 1){
+        listContent.textContent = 'The image "' + productCollection[i].imageCaption + '" recieved ' + productCollection[i].clicked + ' vote out of ' + productCollection[i].shown + ' times shown.';
+        resultsList.appendChild(listContent);
+      }
+    }
+  }
+}
 
