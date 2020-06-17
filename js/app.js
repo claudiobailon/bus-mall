b6ad5e13a69cdc98968873ec4749f6b5e66764d5
claudiobailon <c.bailonschubert@gmail.com>
2020-06-16 19:27:36 -0700

    Added second bar graph displaying times shown

--- js/app.js
+++ js/app.js
@@ -180,6 +180,10 @@ function renderProductChart() {
   for(i = 0; i < Product.collection.length; i++){
     imgClicks.push(Product.collection[i].clicked);
   }
+  var imgShown = [];
+  for(i = 0; i < Product.collection.length; i++){
+    imgShown.push(Product.collection[i].shown);
+  }
 
 
   var ctx = document.getElementById('productChart').getContext('2d');
@@ -192,22 +196,27 @@ function renderProductChart() {
         data: imgClicks,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+
         ],
         borderWidth: 1
-      }]
+      },
+      {
+        label: 'Product Shown',
+        data: imgShown,
+        backgroundColor: [
+
+          'rgba(54, 162, 235, 0.2)',
+
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1
+      }
+      ]
     },
     options: {
       scales: {
